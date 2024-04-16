131bf57029b789fd49640bae35fb6b7f34b1a21f
Jean LE CHAUDELEC <jlechaudelec@gmail.com>
2024-04-16 18:09:23 +0200

    avancée exo2

--- TD5/src/td05_ex02.cpp
+++ TD5/src/td05_ex02.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <cstdlib>
 #include <unordered_map>
+#include <algorithm>
 
 std::string random_name(size_t size) {
     std::string name {""};
@@ -31,13 +32,43 @@ std::vector<std::pair<std::string, float>> get_robots_fix(size_t size) {
 }
 
 
+
 std::unordered_map<std::string, std::vector<float>> robots_fixes_map(std::vector<std::pair<std::string, float>> const& robots_fixes) {
-    
+    std::unordered_map<std::string, std::vector<float>> liste{};
+    for (int i{0}; i < robots_fixes.size(); i++) {
+        std::string name_robot {robots_fixes[i].first};
+        // auto it_tableau1 {std::find_if(robots_fixes.begin(), robots_fixes.end(), name_robot)};
+        auto it_tableau2 {liste.find(name_robot)};
+
+        auto is_name_robot = [name_robot](const std::pair<std::string, float>& pair) {
+            return pair.first == name_robot;
+        };
+
+        if (it_tableau2 == liste.end()) {
+            auto robot_suivant {std::find_if(robots_fixes.begin(), robots_fixes.end(), is_name_robot)};
+            auto robot_precedent{robots_fixes.begin()};
+            std::vector<float>  liste_reparations{};
+            while (robot_suivant != robots_fixes.end()) {
+                robot_suivant = std::find_if(robot_precedent, robots_fixes.end(), is_name_robot);
+                if (robot_suivant != robots_fixes.end()) {
+                    liste_reparations.push_back(robot_suivant->second);
+                }
+                robot_precedent = robot_suivant+1;
+        
+        
+            }
+            liste.insert({name_robot, liste_reparations});
+        }
+    }
+    return liste;
 }
 
 
 int main() {
     std::vector<std::pair<std::string, float>> robots_fixes{get_robots_fix(10)};
-    std::vector<std::pair<std::string, float>> liste{robots_fixes_map(robots_fixes)};
+    std::unordered_map<std::string, std::vector<float>> liste{robots_fixes_map(robots_fixes)};
+    for (int i{}; i < liste.size(); i++) {
+        std::cout << liste[i] << std::endl;
+    }
     return 0;
 }
